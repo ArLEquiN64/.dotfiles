f95f8a75a76da9c184bb4c9478ce3cba6a0487bd
ArLE <ARLEQUIN.prx@gmail.com>
2015-06-02 13:45:01 +0900

    add zprezto files

---
+++ dotfilesLink.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# ---- zprezto ----
+ln -sf ~/.dotfiles/zprezto ~/.zprezto
+ln -sf ~/.dotfiles/zprezto/runcoms/zlogin ~/.zlogin
+ln -sf ~/.dotfiles/zprezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/.dotfiles/zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -sf ~/.dotfiles/zprezto/runcoms/zprofile ~/.zprofile
+ln -sf ~/.dotfiles/zprezto/runcoms/zshenv ~/.zshenv
+ln -sf ~/.dotfiles/zprezto/runcoms/zshrc ~/.zshrc
+
