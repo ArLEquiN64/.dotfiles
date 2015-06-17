725c27ffc2d2eb46138880fbab8709563f63a9f3
ArLE <ARLEQUIN.prx@gmail.com>
2015-06-18 04:13:47 +0900

    move zprezto into zsh

--- link.sh
+++ link.sh
@@ -5,11 +5,11 @@ ln -sf ~/.dotfiles/vim ~/.vim
 ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
 
 # ---- zprezto ----
-ln -sf ~/.dotfiles/zprezto ~/.zprezto
-ln -sf ~/.dotfiles/zprezto/runcoms/zlogin ~/.zlogin
-ln -sf ~/.dotfiles/zprezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/.dotfiles/zsh/zprezto ~/.zprezto
+ln -sf ~/.dotfiles/zsh/zprezto/runcoms/zlogin ~/.zlogin
+ln -sf ~/.dotfiles/zsh/zprezto/runcoms/zlogout ~/.zlogout
 ln -sf ~/.dotfiles/zsh/zpreztorc ~/.zpreztorc
 ln -sf ~/.dotfiles/zsh/zprofile ~/.zprofile
-ln -sf ~/.dotfiles/zprezto/runcoms/zshenv ~/.zshenv
+ln -sf ~/.dotfiles/zsh/zprezto/runcoms/zshenv ~/.zshenv
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 
