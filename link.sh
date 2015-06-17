731a897c23f680afd9dafb89ca5ef02f6d3c064a
ArLE <ARLEQUIN.prx@gmail.com>
2015-06-18 02:02:04 +0900

    separate zsh runcomes

--- link.sh
+++ link.sh
@@ -9,7 +9,7 @@ ln -sf ~/.dotfiles/zprezto ~/.zprezto
 ln -sf ~/.dotfiles/zprezto/runcoms/zlogin ~/.zlogin
 ln -sf ~/.dotfiles/zprezto/runcoms/zlogout ~/.zlogout
 ln -sf ~/.dotfiles/zprezto/runcoms/zpreztorc ~/.zpreztorc
-ln -sf ~/.dotfiles/zprezto/runcoms/zprofile ~/.zprofile
+ln -sf ~/.dotfiles/zsh/zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zprezto/runcoms/zshenv ~/.zshenv
-ln -sf ~/.dotfiles/zprezto/runcoms/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 
