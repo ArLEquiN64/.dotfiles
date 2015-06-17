7b6ed28f49c42dc41f9a48e2e28466796cdb3b77
ArLE <ARLEQUIN.prx@gmail.com>
2015-06-18 02:57:18 +0900

    add zpreztorc

--- link.sh
+++ link.sh
@@ -8,7 +8,7 @@ ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zprezto ~/.zprezto
 ln -sf ~/.dotfiles/zprezto/runcoms/zlogin ~/.zlogin
 ln -sf ~/.dotfiles/zprezto/runcoms/zlogout ~/.zlogout
-ln -sf ~/.dotfiles/zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -sf ~/.dotfiles/zsh/zpreztorc ~/.zpreztorc
 ln -sf ~/.dotfiles/zsh/zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zprezto/runcoms/zshenv ~/.zshenv
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
